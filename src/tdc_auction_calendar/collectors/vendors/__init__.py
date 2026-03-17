9f48d2ce1682cb65a72e450ebc42cacccff00c1a
Mason Porter <68439368+mretrop@users.noreply.github.com>
2026-03-16 22:01:01 -0600

    feat(publicsurplus): export collector from vendors package (#57)

--- src/tdc_auction_calendar/collectors/vendors/__init__.py
+++ src/tdc_auction_calendar/collectors/vendors/__init__.py
@@ -1,6 +1,7 @@
 from tdc_auction_calendar.collectors.vendors.bid4assets import Bid4AssetsCollector
 from tdc_auction_calendar.collectors.vendors.mvba import MVBACollector
+from tdc_auction_calendar.collectors.vendors.publicsurplus import PublicSurplusCollector
 from tdc_auction_calendar.collectors.vendors.purdue import PurdueCollector
 from tdc_auction_calendar.collectors.vendors.realauction import RealAuctionCollector
 
-__all__ = ["Bid4AssetsCollector", "MVBACollector", "PurdueCollector", "RealAuctionCollector"]
+__all__ = ["Bid4AssetsCollector", "MVBACollector", "PublicSurplusCollector", "PurdueCollector", "RealAuctionCollector"]
